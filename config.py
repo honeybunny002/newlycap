20d73b0e4ddcfb93a505768c0be4021a9593b55b
honeybunny002 <104381813+honeybunny002@users.noreply.github.com>
2023-02-02 15:10:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-      API_ID = int(os.environ.get("APP_ID", 12345))
-      API_HASH = os.environ.get("API_HASH")
-      CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
-      ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Ts_Bots")
-      ADMIN_ID = int(os.environ.get("ADMIN_ID", 123476535 )) 
-      DB_URL = os.environ.get("DATABASE_URL", "")
+      BOT_TOKEN = "5634634120:AAF4-pPK3rvRLadLhLCiAVuob7A5ZNuvr7Q"
+      API_ID = 5080899
+      API_HASH = "6fc7f813cf96e6692990b752b43fd9c7"
+      CAPTION_POSITION = "bottom"
+      ADMIN_USERNAME = "hellohoneybunnyy
+      ADMIN_ID = 956022686 
+      DB_URL = "postgres://nmeeirqa:CkgMOWflErlquDUqF-vhsAQw7DqXhLf2@mouse.db.elephantsql.com/nmeeirqa"
