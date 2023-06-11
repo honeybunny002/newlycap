aff9e4fc4f01b4ab0ed47b0dcc88e5c559071b10
honeybunny002 <104381813+honeybunny002@users.noreply.github.com>
2023-06-11 18:08:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ class Config(object):
       API_ID = 5080899
       API_HASH = "6fc7f813cf96e6692990b752b43fd9c7"
       CAPTION_POSITION = "bottom"
-      ADMIN_USERNAME = "hellohoneybunnyy
+      ADMIN_USERNAME = "@hellohoneybunnyy"
       ADMIN_ID = 956022686 
       DB_URL = "postgres://nmeeirqa:CkgMOWflErlquDUqF-vhsAQw7DqXhLf2@mouse.db.elephantsql.com/nmeeirqa"
