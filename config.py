c32de5798e95dc4e0ff5968a0cd79df3fd12f247
honeybunny002 <104381813+honeybunny002@users.noreply.github.com>
2023-06-12 14:55:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ import os
 
 class Config(object):
       BOT_TOKEN = "5634634120:AAF4-pPK3rvRLadLhLCiAVuob7A5ZNuvr7Q"
-      API_ID = 5080899
-      API_HASH = "6fc7f813cf96e6692990b752b43fd9c7"
+      API_ID = 27037347
+      API_HASH = "e8d61b08835c9f11d61bbadec9d7610b"
       CAPTION_POSITION = "bottom"
       ADMIN_USERNAME = "@hellohoneybunnyy"
       ADMIN_ID = 956022686 
